1a17c463596462ed163a03cf582de0dd3b004441
Ivan <carefall.regular@gmail.com>
2026-02-07 00:26:16 +0300

    Testing foldered structure

--- Internals/GitHubAutoUpdate.cs
+++ Internals/GitHubAutoUpdate.cs
@@ -13,71 +13,70 @@ namespace SolyankaGuide.Internals
 
         public static async Task<int> Update(TextBlock status)
         {
+            status.Text = Locale.Get("update_check");
+            var (jsonsDict, imagesDict) = await GetGitHubHashes("carefall", "SolyankaGuide", "Assets/hashes.json");
+            if (jsonsDict == null || imagesDict == null)
+            {
+                MessageBox.Show(Locale.Get("update_time_exceeded"), Locale.Get("update"), MessageBoxButton.OK);
+                return -1;
+            }
+            status.Text = Locale.Get("update_texts");
             var localJsons = GetLocalFiles(@"Assets/Data");
-            var localImages = GetLocalFiles(@"Assets/Images");
-            if (localJsons == null || localImages == null)
+            if (localJsons == null)
             {
                 MessageBox.Show(Locale.Get("disk_check_fail"), Locale.Get("update"), MessageBoxButton.OK);
                 return -1;
             }
-            status.Text = Locale.Get("update_check");
-            List<string> localFiles = new();
-            localFiles.AddRange(localImages);
-            localFiles.AddRange(localJsons);
-            Dictionary<string, string?>? hashes = await GetGitHubHashes("carefall", "SolyankaGuide", "Assets/hashes.json");
-            if (hashes == null || hashes.Count == 0)
+            var localImages = GetLocalFiles(@"Assets/Images");
+            if (localImages == null)
             {
-                MessageBox.Show(Locale.Get("update_time_exceeded"), Locale.Get("update"), MessageBoxButton.OK);
+                MessageBox.Show(Locale.Get("disk_check_fail"), Locale.Get("update"), MessageBoxButton.OK);
                 return -1;
             }
-            status.Text = Locale.Get("update_file_check");
-            Dictionary<string, string> updateFiles = new();
-            var githubJsons = await GetGitHubFolderContents("carefall", "SolyankaGuide", "Assets/Data");
-            var githubImages = await GetGitHubFolderContents("carefall", "SolyankaGuide", "Assets/Images");
-            if (githubJsons == null || githubJsons.Count == 0 || githubImages == null || githubImages.Count == 0) 
+            List<string> filesToDelete = new();
+            Dictionary<string, string> filesToUpdate = new();
+            foreach (string localFile in localJsons)
             {
-                MessageBox.Show(Locale.Get("update_time_exceeded"), Locale.Get("update"), MessageBoxButton.OK);
-                return -1;
+
+                string path = Path.Combine(@"Assets/Data", localFile);
+                if (!jsonsDict.ContainsKey(localFile)) filesToDelete.Add(path);
+                if (jsonsDict[localFile] != ComputeFileSha1(path)) filesToUpdate[localFile] = path;
             }
-            status.Text = Locale.Get("update_comparison");
-            List<GitHubContentItem> githubFiles = new();
-            githubFiles.AddRange(githubImages);
-            githubFiles.AddRange(githubJsons);
-            foreach (var item in githubFiles)
+            foreach (string localFile in localImages)
             {
-                bool needDownload = false;
-                var localFilePath = Path.Combine("Assets", item.Path!.Substring("Assets/".Length).Replace("\\", "/"));
-                if (!File.Exists(localFilePath))
-                {
-                    needDownload = true;
-                }
-                else
-                {
-                    string localSha = ComputeFileSha1(localFilePath);
-                    if (localSha != hashes[item.Name!])
-                    {
-                        needDownload = true;
-                    }
-                }
-                if (needDownload)
-                {
-                    updateFiles.Add(item.Download_url!, localFilePath);
-                }
+                string path = Path.Combine(@"Assets/Images", localFile);
+                if (!jsonsDict.ContainsKey(localFile)) filesToDelete.Add(path);
+                if (jsonsDict[localFile] != ComputeFileSha1(path)) filesToUpdate[localFile] = path;
             }
-            if (updateFiles.Count > 0)
+            foreach(string gitFile in jsonsDict.Keys)
+            {
+                string path = Path.Combine(@"Assets/Images", gitFile);
+                if (!localJsons.Contains(gitFile)) filesToUpdate[gitFile] = path;
+            }
+            foreach (string gitFile in imagesDict.Keys)
+            {
+                string path = Path.Combine(@"Assets/Images", gitFile);
+                if (!localImages.Contains(gitFile)) filesToUpdate[gitFile] = path;
+            }
+            if (filesToUpdate.Count > 0)
             {
                 status.Text = Locale.Get("update_found_status");
-                Logger.Log("Updater", $"Found new version with changed files: {string.Join(" ", updateFiles.Keys)}.");
-                var result = MessageBox.Show(Locale.Get("update_found"), Locale.Get("update"), MessageBoxButton.YesNo);
+                Logger.Log("Updater", $"Found new version."); // add detailed description
+                var result = MessageBox.Show(Locale.Get("update_found"), Locale.Get("update"), MessageBoxButton.YesNo); // add detailed description
                 if (result == MessageBoxResult.Yes)
                 {
                     Logger.Log("Updater", "Installing update.");
-                    var keys = updateFiles.Keys.ToArray();
-                    for(int i = 0; i < keys.Length; i++)
+                    var keys = filesToUpdate.Keys.ToArray();
+                    int len = keys.Length;
+                    for (int i = 0; i < len; i++)
                     {
                         var key = keys[i];
-                        status.Text = Locale.Get("installation", $"{i + 1} / {keys.Length}");
-                        await DownloadFile(key, updateFiles[key]);
+                        status.Text = Locale.Get("installation", $"{i + 1} / {len}");
+                        await DownloadFile(key, filesToUpdate[key]);
+                    }
+                    foreach (var path in filesToDelete)
+                    {
+                        File.Delete(path);
                     }
                     return 1;
                 }
@@ -86,7 +85,7 @@ namespace SolyankaGuide.Internals
             return 0;
         }
 
-        private static async Task<Dictionary<string, string?>?> GetGitHubHashes(string owner, string repo, string path)
+        private static async Task<(Dictionary<string, string>? jsonsDict, Dictionary<string, string>? imagesDict)> GetGitHubHashes(string owner, string repo, string path)
         {
             string url = $"https://raw.githubusercontent.com/{owner}/{repo}/main/{path}";
             using HttpClient client = new()
@@ -98,32 +97,14 @@ namespace SolyankaGuide.Internals
             {
                 string json = await client.GetStringAsync(url);
                 JObject obj = JObject.Parse(json);
-                return obj.Properties().ToDictionary(prop => prop.Name, prop => (string?)prop.Value);
+                var jsonsDict = obj["jsons"]?.ToObject<Dictionary<string, string>>();
+                var imagesDict = obj["images"]?.ToObject<Dictionary<string, string>>();
+                return (jsonsDict, imagesDict);
             }
             catch (Exception ex)
             {
                 Logger.Log("Updater", ex.ToString());
-                return null;
-            }
-        }
-
-        public static async Task<List<GitHubContentItem>?> GetGitHubFolderContents(string owner, string repo, string path)
-        {
-            try
-            {
-                var url = $"https://api.github.com/repos/{owner}/{repo}/contents/{path}";
-                using var client = new HttpClient()
-                {
-                    Timeout = TimeSpan.FromSeconds(10)
-                };
-                client.DefaultRequestHeaders.UserAgent.ParseAdd("SolyankaGuide");
-                var response = await client.GetStringAsync(url);
-                return JsonConvert.DeserializeObject<List<GitHubContentItem>>(response);
-            }
-            catch (Exception ex)
-            {
-                Logger.Log("Updater", ex.ToString());
-                return null;
+                return (null, null);
             }
         }
 
@@ -148,7 +129,7 @@ namespace SolyankaGuide.Internals
             }
         }
 
-        public static async Task DownloadFile(string url, string destinationPath)
+        public static async Task DownloadFile(string file, string destinationPath)
         {
             try
             {
@@ -156,7 +137,7 @@ namespace SolyankaGuide.Internals
                 {
                     Timeout = TimeSpan.FromSeconds(10)
                 };
-                var data = await client.GetByteArrayAsync(url);
+                var data = await client.GetByteArrayAsync($"https://raw.githubusercontent.com/carefall/SolyankaGuide/main/Assets/{file}");
                 Directory.CreateDirectory(Path.GetDirectoryName(destinationPath)!);
                 await File.WriteAllBytesAsync(destinationPath, data);
             }
@@ -165,15 +146,6 @@ namespace SolyankaGuide.Internals
                 Logger.Log("Updater", ex.ToString());
                 MessageBox.Show(Locale.Get("install_fail"), Locale.Get("update"), MessageBoxButton.OK);
             }
-        }
-
-        public class GitHubContentItem
-        {
-            public string? Name { get; set; }
-            public string? Path { get; set; }
-            public string? Sha { get; set; }
-            public string? Type { get; set; }
-            public string? Download_url { get; set; }
         }
     }
 }
