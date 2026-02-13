bac8c5c780192a29491f70611a786af6caa00467
Ivan <carefall.regular@gmail.com>
2026-02-13 20:54:03 +0300

    New Grid UI

--- GriddedDescription.xaml.cs
+++ GriddedDescription.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
 namespace SolyankaGuide
 {
     public partial class GriddedDescription : UserControl
@@ -6,6 +9,11 @@ namespace SolyankaGuide
         public GriddedDescription()
         {
             InitializeComponent();
+        }
+
+        private void Image_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            DescGrid.Clip = new RectangleGeometry(new Rect(0, 0, Image.ActualWidth, Image.ActualHeight), 10, 10);
         }
     }
 }
